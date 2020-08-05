1836975cf1bc290d8e642dc2f56a02b708daf855
mikiteek <mikiteek@gmail.com>
2020-08-05 20:43:30 +0300

    fixed bug with unknown country

--- src/js/fetchQuery.js
+++ src/js/fetchQuery.js
@@ -2,6 +2,11 @@
 function fetchCountries(searchQuery) {
  const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
  return fetch(url)
-   .then(response => response.json())
+   .then(response => {
+    if (response.status !== 200) {
+     return 404;
+    }
+    return response.json()
+   })
 }
 export default fetchCountries;
